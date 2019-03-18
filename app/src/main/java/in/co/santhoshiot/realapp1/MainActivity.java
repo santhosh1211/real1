07706eb006989c3bf754c3d51c717b18636143f5
santhosh1211 <santhosh10dm43@gmail.com>
2019-03-18 06:51:54 +0530

    Initial commit

---
+++ app/src/main/java/in/co/santhoshiot/realapp1/MainActivity.java
@@ -0,0 +1,13 @@
+package in.co.santhoshiot.realapp1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
